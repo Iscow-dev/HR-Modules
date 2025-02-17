a8c32cb9f266dab2581802277f5e1023a4964029
Iscow-dev <pelinghefrancisco@gmail.com>
2025-02-17 13:29:17 +0800

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,7 +1,24 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-import pandas as pd
-import os
-from datetime import datetime
-import io
-from st_aggrid import AgGrid, GridOptionsBuilder
+
+
+
+#LOGIN
+# Apply custom CSS to adjust the size of the text input fields
+page_bg_img = f"""
+<style>
+
+.st-emotion-cache-1yiq2ps {{
+    background-image: url("https://images.unsplash.com/photo-1735908235870-f4dd182a2f12?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg");        
+    background-size: cover;
+}}
+.st-emotion-cache-h4xjwg {{
+    background-color: rgba(0, 0, 0, 0);
+}}
+.st-emotion-cache-pb6fr7 {{
+    width: 380px;
+}}
+
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
